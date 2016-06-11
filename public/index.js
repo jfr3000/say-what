e5323fca4b7507f06bdf9befafdd35cf43c5e7aa
Julia Friesel <julia.friesel@gmail.com>
2016-06-11 16:28:05 +0200

    user feedback

--- public/index.js
+++ public/index.js
@@ -1,13 +1,26 @@
 "use strict";
 
 let recognition = new webkitSpeechRecognition();
+let statusText = document.getElementById('status');
 
-recognition.start();
+recognition.onstart = function() {
+    statusText.innerHTML = "I'm all ears";
+};
+
+recognition.onspeechstart = function() {
+    statusText.innerHTML = "So interesting, tell me more!";
+};
 
 recognition.onresult = function(result) {
     result = result.results[0][0];
     let echo = `I am ${Math.round(result.confidence*100)}% sure that you said: ${result.transcript}`;
+    statusText.innerHTML = result.transcript;
     let spokenEcho = new SpeechSynthesisUtterance(echo);
     spokenEcho.lang = 'en-US';
+    spokenEcho.onend = function() {
+        recognition.start();
+    };
     window.speechSynthesis.speak(spokenEcho);
 };
+
+recognition.start();
